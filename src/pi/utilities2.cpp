2dc5ccbcee6367ca7bca3826fc32fa8fc513e6a8
Ivan Kuchin <ivan.kuchin@gmail.com>
2021-04-14 16:17:53 -0400

    minor change

--- src/pi/utilities2.cpp
+++ src/pi/utilities2.cpp
@@ -12,7 +12,7 @@ auto	GetSHA512(const string &src) -> string
     SHA512_Update(&ctx, src.c_str(), src.length());
     SHA512_Final(digest, &ctx);
 
-    char mdString[SHA512_DIGEST_LENGTH*2+ 1 ];   /* Flawfinder: ignore */
+    char mdString[SHA512_DIGEST_LENGTH*2+1];   /* Flawfinder: ignore */
     for (int i = 0; i < SHA512_DIGEST_LENGTH; i++)
 		sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);    /* Flawfinder: ignore */
 
